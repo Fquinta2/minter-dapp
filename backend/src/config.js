f2e40d965e8ebc93c0956a0d7894d808b0dacd92
MetaCrossEliteClub <fquintanilha@protonmail.com>
2022-04-21 23:32:59 -0300

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,24 +7,83 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "MetaCrossEliteClub";
+const description = "The MetaCross Elite Club is first NFTs collection of the hardest sport of world.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 1000,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: 'Background'},
+      { name: 'Man White Body'},
+      { name: 'Man White Head'},
+      { name: 'Man Mouth'},
+      { name: 'Man Eye'},
+    { name: 'Headset'},
+      { name: 'Man Cap'},
+      { name: 'Shake'},
+    { name: 'Glasses'},
     ],
-  },
+  },{
+    growEditionSizeTo: 2000,
+    layersOrder: [
+      { name: 'Background'},
+      { name: 'Man Brown Body'},
+      { name: 'Man Brown Head'},
+      { name: 'Man Mouth'},
+      { name: 'Man Eye'},
+    { name: 'Headset'},
+      { name: 'Man Cap'},
+      { name: 'Shake'},
+    { name: 'Glasses'},
+    ],
+  },{
+    growEditionSizeTo: 3000,
+    layersOrder: [
+      { name: 'Background'},
+      { name: 'Girl Body'},
+      { name: 'Girl Head'},
+      { name: 'Girl Move'},
+      { name: 'Girl Mouth'},
+      { name: 'Girl Eye'},
+    { name: 'Headset'},
+    { name: 'Girl Cap'},
+      { name: 'Shake'},
+    { name: 'Glasses'},
+    ],
+  },{
+    growEditionSizeTo: 3500,
+    layersOrder: [
+      { name: 'Background'},
+      { name: 'Man White Body B'},
+      { name: 'Man Tshirt'},
+      { name: 'Chain'},
+      { name: 'Man White Head'},
+      { name: 'Man Mouth'},
+      { name: 'Man Eye'},
+    { name: 'Headset'},
+      { name: 'Man Cap'},
+      { name: 'Shake'},
+    { name: 'Glasses'},
+    ],
+  },{
+    growEditionSizeTo: 4000,
+    layersOrder: [
+      { name: 'Background'},
+      { name: 'Man Brown Body B'},
+      { name: 'Man Tshirt'},
+      { name: 'Chain'},
+      { name: 'Man Brown Head'},
+      { name: 'Man Mouth'},
+      { name: 'Man Eye'},
+    { name: 'Headset'},
+      { name: 'Man Cap'},
+      { name: 'Shake'},
+    { name: 'Glasses'},
+    ],
+  }
 ];
 
 const shuffleLayerConfigurations = true;
@@ -32,13 +91,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 720,
+  height: 720,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://themetacross.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,22 +108,22 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'MetaCrossEliteClub';
+const CONTRACT_SYMBOL = 'TMEC';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0x621ebA2c4A5f703dAa02eBd8d6eF3E46Aab7C1Aa';
+const TREASURY_ADDRESS = '0x621ebA2c4A5f703dAa02eBd8d6eF3E46Aab7C1Aa';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.003; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-22T03:15:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_SHARE = 400; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0x621ebA2c4A5f703dAa02eBd8d6eF3E46Aab7C1Aa"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -75,8 +134,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "The first NFTs collection of the hardest sport of the world."; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreif35wbkb4ehyfwvso4ssax27gzyjik4zh5fawjyml5drmcm3zbd5u"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xBfbe23a8a6987b529fE84A580d107f7C70D3B2b7"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
